63f7e8d87a268fa32e4b20cf71c285fef4029cc7
blur <damienhebert710@gmail.com>
2022-07-24 22:13:43 -0500

    rewriting code within the server, messing with apollo and the auth middleware

--- server/server.js
+++ server/server.js
@@ -6,13 +6,26 @@ const {ApolloServer} = require('apollo-server-express');
 const {authMiddleware} = require('./utils/auth');
 
 
+
+
 const path = require('path');
+const {typeDefs, resolvers } = require('./schemas')
+
+
 const db = require('./config/connection');
 const routes = require('./routes');
 
-const app = express();
+
 const PORT = process.env.PORT || 3001;
 
+const server = new ApolloServer({
+  typeDefs,
+  resolvers, 
+  context: authMiddleware, 
+});
+
+
+const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
